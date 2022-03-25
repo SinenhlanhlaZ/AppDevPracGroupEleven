b6c0f5d826d5a02daf7cb15cea64b3880a5cbe79
Sinenhlanhla Zondi <sinezondi17@gmail.com>
2022-03-25 20:37:59 +0200

    added name to java files

---
+++ src/main/java/za/ac/cput/entities/Doctor.java
@@ -0,0 +1,12 @@
+package za.ac.cput.entities;
+
+/*
+    Doctor.java
+    Entity for the Doctors
+    Author: Sinenhlanhla Zondi (220008922)
+    Date: 25 March 2022
+*/
+
+public class Doctor {
+
+}
