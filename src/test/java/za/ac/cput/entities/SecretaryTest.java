d6d033b0674bef3e2b0605f13dc0b13a532c6efa
Sinenhlanhla Zondi <sinezondi17@gmail.com>
2022-03-30 18:48:57 +0200

    deleted code from SecretaryTest

--- src/test/java/za/ac/cput/entities/SecretaryTest.java
+++ src/test/java/za/ac/cput/entities/SecretaryTest.java
@@ -7,18 +7,6 @@ package za.ac.cput.entities;
     Date: 25 March 2022
 */
 
-import org.junit.jupiter.api.Test;
-import za.ac.cput.factory.SecretaryFactory;
-import za.ac.cput.factory.SecretaryID;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class SecretaryTest {
 
-    @Test
-    public void testSecretary() {
-        SecretaryFactory secFactory = new SecretaryFactory();
-        SecretaryID sec = secFactory.getSecretaryName("2433");
-        sec.secretaryID();
-    }
 }
