8d401fec7f1c00d4286a8a811ef2794d7075ff8c
NonzwakaziMgxaji <nonzwakazimgxaji@gmail.com>
2022-03-30 09:40:29 +0200

    Added Driver and Supplier entity and factory

---
+++ src/test/java/za/ac/cput/factory/DriverFactoryTest.java
@@ -0,0 +1,13 @@
+package za.ac.cput.factory;
+
+/*
+        DriverFactoryTest.java
+        Author: Nonzwakazi Mgxaji (213181584)
+        Date: 28 March 2022
+ */
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DriverFactoryTest {
+
+}
