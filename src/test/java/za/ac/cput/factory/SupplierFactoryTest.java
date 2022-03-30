cacd5492518008566f6f41188db4f7ed6c5cce57
NonzwakaziMgxaji <nonzwakazimgxaji@gmail.com>
2022-03-30 17:05:31 +0200

    Driver and Supplier entities and factories

--- src/test/java/za/ac/cput/factory/SupplierFactoryTest.java
+++ src/test/java/za/ac/cput/factory/SupplierFactoryTest.java
@@ -7,9 +7,15 @@ package za.ac.cput.factory;
  */
 
 import org.junit.jupiter.api.Test;
+import za.ac.cput.entities.Supplier;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SupplierFactoryTest {
-
+    @Test
+    public void test() {
+        Supplier supplier = SupplierFactory.createSupplier("01", "61 Loop Street Cape Town 8000", 777888);
+        System.out.println(supplier);
+    }
 }
