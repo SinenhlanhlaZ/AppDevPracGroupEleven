e4a45c4c230e38afb1c45e6e777b73123482ae65
Fayaad Abrahams <80263556+FayaadAbrahams@users.noreply.github.com>
2022-03-30 09:37:19 +0200

    Updated HospitalRoomFactory and NurseFactory files.
    Included space for Util package methods.

--- src/main/java/za/ac/cput/factory/NurseFactory.java
+++ src/main/java/za/ac/cput/factory/NurseFactory.java
@@ -10,10 +10,12 @@ import za.ac.cput.entities.Nurse;
 public class NurseFactory {
 
     public static Nurse createNurse(String nurseID, String nurseFirstName, String nurseLastName) {
-        if (nurseFirstName.equals("")) {
+        // Generate Method to create nurseID in Helper package
+
+        if (nurseFirstName.equals("") || nurseFirstName == null) {
             return null;
         }
-        if (nurseLastName.equals("")) {
+        if (nurseLastName.equals("") || nurseFirstName == null) {
             return null;
         }
         return new Nurse.Builder().setNurseID(nurseID)
