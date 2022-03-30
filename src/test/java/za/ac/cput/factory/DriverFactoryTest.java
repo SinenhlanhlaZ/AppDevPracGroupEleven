7dbe84959808e89bbcb6f1aacbfb740d9de5ee06
NonzwakaziMgxaji <nonzwakazimgxaji@gmail.com>
2022-03-30 17:05:31 +0200

    Driver and Supplier entities and factories

--- src/test/java/za/ac/cput/factory/DriverFactoryTest.java
+++ src/test/java/za/ac/cput/factory/DriverFactoryTest.java
@@ -6,8 +6,17 @@ package za.ac.cput.factory;
         Date: 28 March 2022
  */
 
+import org.junit.jupiter.api.Test;
+import za.ac.cput.entities.Driver;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DriverFactoryTest {
+    @Test
+    public void test() {
+        Driver driver = DriverFactory.createDriver("01", "Lunga", "Tshabalala", 456789);
+        System.out.println(driver);
+
+    }
 
 }
