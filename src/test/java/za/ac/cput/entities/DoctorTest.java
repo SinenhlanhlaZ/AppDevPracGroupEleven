7e5afba03214c285a2022b13270e003209e07b6c
Sinenhlanhla Zondi <sinezondi17@gmail.com>
2022-03-30 18:41:07 +0200

    modified & moved docotr test files

--- src/test/java/za/ac/cput/entities/DoctorTest.java
+++ src/test/java/za/ac/cput/entities/DoctorTest.java
@@ -7,16 +7,6 @@ package za.ac.cput.entities;
     Date: 25 March 2022
 */
 
-import org.junit.jupiter.api.Test;
-import za.ac.cput.factory.DoctorFactory;
-import za.ac.cput.factory.DoctorNames;
-
 class DoctorTest {
 
-    @Test
-    public void testDoctor() {
-        DoctorFactory docFactory = new DoctorFactory();
-        DoctorNames docNames = docFactory.getDoctorFirstName("Sine");
-        docNames.doctorFirstNames();
-    }
 }
