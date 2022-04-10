28148ba148a34588b0a271bb25cfa02ac64e6b38
sinazo-mehlomakhulu <sinazomehlomakhulu@gmail.com>
2022-04-10 13:34:35 +0200

    I added test cases repository and factory

--- src/test/java/za/ac/cput/factory/AppointmentFactoryTest.java
+++ src/test/java/za/ac/cput/factory/AppointmentFactoryTest.java
@@ -1,4 +1,21 @@
+package za.ac.cput.factory;
+/*
+        Appointment factory test
+        Author : Sinazo Mehlomakhulu (216076498)
+        Date: 10 April 2022
+        */
+import org.junit.jupiter.api.Test;
+import za.ac.cput.entities.Appointment;
+import za.ac.cput.entities.Driver;
+
 import static org.junit.jupiter.api.Assertions.*;
-class AppointmentFactoryTest {
-  
+
+public class AppointmentFactoryTest {
+
+    @Test
+  public   void createAppointment() {
+
+       Appointment appointment = AppointmentFactory.createAppointment("20Feb2022", "30Min", "13:00");
+        System.out.println(appointment);
+    }
 }
